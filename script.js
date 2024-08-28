018363d055320f3efe468b16cc0fce35ee424459
denyskh4991 <denyskh4991@gmail.com>
2024-08-28 22:34:00 +0200

    Developing fucntion that validates name

--- script.js
+++ script.js
@@ -7,7 +7,14 @@ let submitError = document.getElementById('submit-error');
 function validateName() {
     let name = document.getElementById('contact-name').value;
 
-    if(name.length == 0) {
+    if (name.length == 0) {
         nameError.innerHTML = 'Name is required';
+        return false;
     }
+    if (!name.match(/^[A-Za-z]+\s[A-Za-z]+$/)) {
+        nameError.innerHTML = 'Write full name';
+        return false;
+    }
+    nameError.innerHTML = 'valid';
+    return true;
 }
